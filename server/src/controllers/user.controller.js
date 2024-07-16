3e246f815c0ba3c45c831adbbeac766898e1646e
amrutgawade <amrutgawade.23@gmail.com>
2024-07-16 19:07:19 +0530

    feat: update user added

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -29,10 +29,9 @@ const addUser = asyncHandler(async (req, res) => {
     )
 })
 
-
 const getUser = asyncHandler(async (req, res) => {
-    const { userName, userEmail } = req.body
-    const user = await User.findOne({ userName, userEmail })
+    const { id } = req.params
+    const user = await User.findOne({ _id: id })
     if (user == null) {
         throw new ApiError(404, "User not found..!")
     }
@@ -42,4 +41,33 @@ const getUser = asyncHandler(async (req, res) => {
     )
 })
 
-export { addUser, getUser }
+const getAllUsers = asyncHandler(async (req, res) => {
+    const allUser = await User.find()
+    if (allUser.length < 1) {
+        throw new ApiError(404, "Users not found..!")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, allUser, "User found..!")
+    )
+})
+
+const updateUser = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const { userName, userEmail, userMobile, userAddress, userCity, userState, userZipCode } = req.body
+    const oldEmail = await User.findById(id)
+
+    if (!(oldEmail.userEmail == userEmail)) {
+        const emailTaken = await User.findOne({ userEmail })
+        if (emailTaken) {
+            throw new ApiError(400, "Email Taken Already..!")
+        }
+    }
+    const user = await User.findByIdAndUpdate(id, { userName, userEmail, userMobile, userAddress, userCity, userState, userZipCode })
+
+    return res.status(201).json(
+        new ApiResponse(200, user, "User found..!")
+    )
+})
+
+export { addUser, getUser, getAllUsers, updateUser }
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { addUser, getUser } from "../controllers/user.controller.js";
+import { addUser, getUser, getAllUsers, updateUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
 router.route("/addUser").post(addUser)
-router.route("/getUser").post(getUser)
+router.route("/users/:id").get(getUser)
+router.route("/getAllUsers").get(getAllUsers)
+router.route("/users/:id").put(updateUser)
 
 export default router
