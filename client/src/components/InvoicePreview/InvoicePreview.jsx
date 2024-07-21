d4f86b7ba8b4b17b80eb9de86b6ce10603cd27e3
amrutgawade <amrutgawade.23@gmail.com>
2024-07-22 01:15:08 +0530

    feat: Invoice Preview and Download feature completed

--- client/src/components/InvoicePreview/InvoicePreview.jsx
+++ client/src/components/InvoicePreview/InvoicePreview.jsx
@@ -1,113 +1,118 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-import "./InvoicePreview.css";
 
 function NewInvoice({ data, user }) {
   return (
-    <div className="invoice_container">
-      <div className="header">
-        <p>INVOICE</p>
-        <div className="h-line" />
-      </div>
-      <div className="section-1">
-        <div className="company_logo">
-          <img src={logo} alt="Logo" className="logo" />
+    <div id="invoice" className="p-8">
+      <h1 className="mb-5 text-center text-[2em] leading-none">INVOICE</h1>
+      <hr className="border-t-4 border-black leading-none" />
+
+      <div className="outline-none border-b border-b-black/50 flex flex-wrap justify-between mx-8 py-6">
+        <div className="flex gap-4">
+          <img
+            src={logo}
+            className="self-start mt-1"
+            alt="FreakDevs Logo"
+            height={60}
+            width={60}
+          />
+          <div className="text-xs tracking-wider leading-relaxed">
+            <p className="text-lg font-bold">FreakDevs</p>
+            <p>Business Number 8237941681</p>
+            <p>Banda</p>
+            <p>Maharashtra</p>
+            <p>416514</p>
+            <p>thefreakdevs@gmail.com</p>
+          </div>
         </div>
-        <div className="company_info">
-          <p>
-            <b>FreakDevs</b>
-          </p>
-          <p>Business Number 8237941681</p>
-          <p>Banda</p>
-          <p>Maharashtra</p>
-          <p>416514</p>
-          <p>7741941681</p>
-          <p>thefreakdevelopers@gmail.com</p>
-        </div>
-        <div className="invoice_no">
-          <b>INVOICE</b>
-          <p>{data?.invoiceNumber}</p>
-          {/* <p>INV0001</p> */}
-          <b>DATE</b>
-          <p>{data?.invoiceDate?.substring(0, 10)}</p>
-          <b>BALANCE DUE</b>
-          <p>INR ₹{data?.balanceDue}</p>
-        </div>
-      </div>
-      <hr />
-      <div className="section-2">
-        <div className="customer_info">
-          <p>BILL TO</p>
-          <p>
-            <b>{user?.userName}</b>
-          </p>
-          <p>{user?.userAddress}</p>
-          <p>
-            {user?.userCity +
-              ", " +
-              user?.userState +
-              " - " +
-              user?.userZipCode}
-          </p>
-          <p>{user?.userMobile}</p>
-          <p>{user?.userEmail}</p>
+        <div className="text-xs tracking-wider text-right flex flex-col justify-between">
+          <div>
+            <b>INVOICE</b>
+            <p>{data?.invoiceNumber}</p>
+          </div>
+          <div>
+            <b>DATE</b>
+            <p>{data?.invoiceDate?.substring(0, 10)}</p>
+          </div>
+          <div>
+            <b>BALANCE DUE</b>
+            <p>₹{data?.balanceDue}</p>
+          </div>
         </div>
       </div>
-      <hr />
-      <div className="section-3">
-        <div className="items_desk">
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>DESCRIPTION</th>
-                <th>RATE</th>
-                <th>QTY</th>
-                <th>AMT</th>
+
+      <div className="mx-8 my-4 text-xs tracking-wider w-max">
+        <p>BILL TO :</p>
+        <b className="text-sm">{user?.userName}</b>
+        <p>{user?.userAddress}</p>
+        <p>
+          {user?.userCity + ", " + user?.userState + " - " + user?.userZipCode}
+        </p>
+        <p>{user?.userMobile}</p>
+        <p>{user?.userEmail}</p>
+      </div>
+
+      <div className="mx-8 flex flex-col">
+        <table className="w-full text-xs tracking-wider">
+          <thead className="text-center align-middle border-y border-y-black/50">
+            <tr>
+              <th className="px-2 py-3 align-middle w-[7%]">ID</th>
+              <th className="px-2 py-3 align-middle w-[63%] text-left">
+                DESCRIPTION
+              </th>
+              <th className="px-2 py-3 align-middle w-[10%]">RATE</th>
+              <th className="px-2 py-3 align-middle w-[10%]">QUANTITY</th>
+              <th className="px-2 py-3 align-middle w-[10%]">AMOUNT</th>
+            </tr>
+          </thead>
+          <tbody className="text-center align-middle divide-y-2 divide-dashed border-b border-b-black/50">
+            {data?.invoiceItems?.map((item, idx) => (
+              <tr key={idx}>
+                <td className="px-2 py-3 align-middle w-[7%]">{idx + 1}</td>
+                <td className="px-2 py-3 align-middle w-[63%] text-left">
+                  {item.description}
+                </td>
+                <td className="px-2 py-3 align-middle w-[10%]">₹{item.rate}</td>
+                <td className="px-2 py-3 align-middle w-[10%]">
+                  {item.quantity}
+                </td>
+                <td className="px-2 py-3 align-middle w-[10%]">
+                  ₹{item.amount}
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {data?.invoiceItems?.map((item, idx) => (
-                <tr key={idx}>
-                  <td>{idx + 1}</td>
-                  <td>{item.description}</td>
-                  <td>₹{item.rate}</td>
-                  <td>{item.quantity}</td>
-                  <td>₹{item.amount}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="items_total">
-          <div className="total">
-            <div className="w-76">
+            ))}
+          </tbody>
+        </table>
+        <div className="w-[50%] self-end">
+          <div className="flex justify-between items-center text-xs tracking-wider border-b border-b-black/30">
+            <div className="w-[78%] text-left font-semibold py-2">
               <p>TOTAL</p>
             </div>
-            <div className="w-24">
+            <div className="w-[22%] text-center">
               <p>₹{data?.amountTotal}</p>
             </div>
           </div>
-          <div className="total">
-            <div className="w-76">
+          <div className="flex justify-between items-center text-xs tracking-wider border-b border-b-black/30">
+            <div className="w-[78%] text-left font-semibold py-2">
               <p>BALANCE PAID</p>
             </div>
-            <div className="w-24">
+            <div className="w-[22%] text-center">
               <p>₹{data?.amountPaid}</p>
             </div>
           </div>
-          <div className="total">
-            <div className="w-76">
+          <div className="flex justify-between items-center text-xs tracking-wider border-b border-b-black/30">
+            <div className="w-[78%] text-left font-semibold py-2">
               <p>BALANCE DUE</p>
             </div>
-            <div className="w-24">
+            <div className="w-[22%] text-center">
               <p>₹{data?.balanceDue}</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="section-4">
-        <b>NOTE</b>
+
+      <div className="mt-8 mx-8 text-xs tracking-wider leading-normal">
+        <b>NOTE :</b>
         <p>{data?.note}</p>
       </div>
     </div>
--- client/src/components/InvoicePreview/InvoicePreviewModal.jsx
+++ client/src/components/InvoicePreview/InvoicePreviewModal.jsx
@@ -1,21 +1,60 @@
 import React from "react";
 import InvoicePreview from "../InvoicePreview/InvoicePreview";
+import html2pdf from "html2pdf.js";
 
 function InvoicePreviewModal({ open, onClose, data, user }) {
+  const downloadPdfHandler = () => {
+    const input = document.getElementById("invoice");
+    const opt = {
+      margin: 0,
+      filename: data.invoiceNumber,
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
+    html2pdf()
+      .set(opt)
+      .from(input)
+      .toPdf()
+      .get("pdf")
+      .then((pdf) => {
+        pdf.setProperties({
+          title: data.invoiceNumber,
+          subject: "Invoice Details",
+          author: "Freakdevs",
+          keywords: "invoice, finance",
+          creator: "Freakdevs-Invoice",
+        });
+      })
+      .save();
+  };
   return (
     <div
       onClick={onClose}
       className={`fixed overflow-y-auto inset-0 justify-center items-center transition-colors ${
-        open ? "visible bg-black/30" : "invisible"
+        open ? "visible bg-black/20" : "invisible"
       }`}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className={`my-8 mx-auto w-[793.7px] h-[1122.5px] bg-white shadow p-6 transition-all ${
+        className={`my-8 mx-auto w-[793.7px] h-[1122.5px] bg-white shadow p-0 transition-all ${
           open ? "scale-100 opacity-100" : "scale-125 opacity-0"
         }`}
       >
         <InvoicePreview user={user} data={data} />
+      </div>
+
+      <div
+        className={`flex justify-end mx-auto mb-8 -mt-4 w-[793.7px] shadow transition-all ${
+          open ? "scale-100 opacity-100" : "scale-125 opacity-0"
+        }`}
+      >
+        <button
+          onClick={downloadPdfHandler}
+          className="px-3 py-2 bg-green-500 text-white rounded-md"
+        >
+          Download Invoice
+        </button>
       </div>
     </div>
   );
